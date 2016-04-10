786fb9e3e6f76a4bbe2d885c09534e811755cbe0
nassim <nassim@nassims-mac.local>
2016-04-11 01:49:28 +0200

    testDay

--- FisuTests/DayTest.swift
+++ FisuTests/DayTest.swift
@@ -33,6 +33,7 @@ class DayTest: XCTestCase {
         }
     }
     
+    // test de la fonction qui insere des nouveau day
     func testDayInsert()
     {
         let moc = (UIApplication.sharedApplication().delegate as!AppDelegate).managedObjectContext
@@ -46,21 +47,45 @@ class DayTest: XCTestCase {
          
     }
     
+    
+    // test de la fonction qui renvoie toutes les activités d'un jour donné
     func testGetActivityFetchedResultController()
     {
+        
+        
         let moc = (UIApplication.sharedApplication().delegate as!AppDelegate).managedObjectContext
-        
         let day8 = Day.insertNewDay(moc, id: 8, day: "Day 8")
-        let activityDay8 = Day.getActivityFetchedResultController("Activity", key: "idAct", predicat: "dayIs=%@", args: (day8)!)
-            XCTAssertNil(activityDay8.sections, "il n ya pas d activite pour Day8")
+        let act = Activity.insertNewActivity(moc, id: 100, nom: "test", desc: "testing", dateD: "04/08/2016, 12:00", dateF: "04/08/2016, 13:00", lieu: nil, speak:  nil , photo: "McDonald's" , day: day8, selected: false)
+        XCTAssertEqual(act?.dayIs?.day, "Day 8",  "L'insertion de act c'est bien passee")
+       let  frc = Day.getActivityFetchedResultController("Activity", key: "idAct", predicat: "dayIs=%@", args: day8!)
         
+        do {
+            try frc.performFetch()
+        } catch {
+            
+            print("An error occured")
+        }
+        let sections = frc.sections
+        let currentSection = sections![0]
+        
+        XCTAssertEqual( currentSection.numberOfObjects, 1,   "il n ya pas d activite pour Day8")
+  
     }
     
+    // test de la fonction qui renvoie tous les jours existants dans la base
     func testGetDayFetchedResultController()
     {
-        
-        let frc = Day.getDayFetchedResultController("Day", key: "keyDay")
-        XCTAssertNotNil(frc)
+       let   frc = Day.getDayFetchedResultController("Day", key: "keyDay")
+        do {
+            try frc.performFetch()
+        } catch {
+            
+            print("An error occured")
+        }
+        let sections = frc.sections
+        let currentSection = sections![0]
+    // On avait 6 day à la base , comme on insere un autre dans la fonction precedente on se retrouve avec 7 day
+    XCTAssertEqual( currentSection.numberOfObjects, 7,   "le nombre de jour existant est faux")
         
         
     }
